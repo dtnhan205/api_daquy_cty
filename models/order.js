d30a152872292c55f151f1102fd807bc72b16a6e
bear080604 <phuonganhkhanh683@gmail.com>
2025-06-25 15:44:09 +0700

    fix checkout

--- models/order.js
+++ models/order.js
@@ -12,9 +12,11 @@ const orderSchema = new mongoose.Schema({
   address: { type: String, required: true },
   orderNote: { type: String },
   products: [{
-    productName: String,
-    size: String,
-    subTotal: Number
+    productId: { type: String, required: true },
+    productName: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true }
+    // Nếu muốn lưu thêm size_name thì thêm: size_name: String
   }],
   totalAmount: { type: Number, required: true },
   grandTotal: { type: Number, required: true },
